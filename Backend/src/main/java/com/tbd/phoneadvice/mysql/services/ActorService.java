8ae58783b968e11ddb33065dd6fcdfb61491bd28
awd <asd>
2019-04-22 18:07:17 -0400

    Backend listo para ser consumido por front

--- Backend/src/main/java/com/tbd/phoneadvice/mysql/services/ActorService.java
+++ Backend/src/main/java/com/tbd/phoneadvice/mysql/services/ActorService.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/actor")
+@CrossOrigin(origins = "localhost:8080", methods= {RequestMethod.GET,RequestMethod.POST})
 public class ActorService {
 
     @Autowired
