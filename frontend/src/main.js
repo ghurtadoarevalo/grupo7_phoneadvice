f483adf2a6d0e1a08de35a75d2f716f5181c9da7
AlejandroOrostica <alejandro.orostica@usach.cl>
2019-05-27 02:01:39 -0400

    grafico de muestra añadido

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import '@babel/polyfill'
 import Vue from 'vue'
+import HighchartsVue from 'highcharts-vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
@@ -8,9 +9,11 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
+Vue.use(HighchartsVue)
 
 new Vue({
   router,
   store,
+  HighchartsVue,
   render: h => h(App)
 }).$mount('#app')
