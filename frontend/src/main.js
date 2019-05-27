8732de6590d5a2dd4ee7f8e01e4aa856e2443340
Gustavo <gustavo.hurtado@usach.cl>
2019-05-27 10:25:11 -0400

    Fix del frontend en develop

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,19 +8,6 @@ import store from './store'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-Vue.use(Vuetify,
-  {
-    theme: {
-      primary: '#1976D2',
-      secondary: '#424242',
-      accent: '#82B1FF',
-      error: '#FF5252', 
-      info: '#2196F3',
-      success: '#4CAF50',
-      warning: '#FFC107'
-  }
-})
-
 Vue.config.productionTip = false
 Vue.use(HighchartsVue)
 
