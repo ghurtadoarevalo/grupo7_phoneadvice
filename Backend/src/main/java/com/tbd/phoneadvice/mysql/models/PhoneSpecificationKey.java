73bade762547fa1fe327ecf550edd8016780179d
Gustavo <gustavo.hurtado@usach.cl>
2019-06-09 02:09:12 -0400

    fixed properties

--- Backend/src/main/java/com/tbd/phoneadvice/mysql/models/PhoneSpecificationKey.java
+++ Backend/src/main/java/com/tbd/phoneadvice/mysql/models/PhoneSpecificationKey.java
@@ -1,10 +1,13 @@
 package com.tbd.phoneadvice.mysql.models;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
+@Data
 public class PhoneSpecificationKey implements Serializable {
 
     @Column(name = "phone_id", unique = true, nullable = false)
