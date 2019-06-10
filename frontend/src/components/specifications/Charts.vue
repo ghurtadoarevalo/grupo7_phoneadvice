708817912389f417f6bfbdce9f5bd14eaec53176
Gustavo <gustavo.hurtado@usach.cl>
2019-06-09 23:34:22 -0400

    fix más que pequeño al front

--- frontend/src/components/specifications/Charts.vue
+++ frontend/src/components/specifications/Charts.vue
@@ -17,7 +17,8 @@
       <v-flex md1>      
         <VBtn fab dark color="#0E318A" @click="allGamma">
           <v-icon large color="white">monetization_on</v-icon>
-        </VBtn> Filtro Gama
+        </VBtn> 
+        <br>Gama
         <v-switch v-model="gammas[0]" @change="filterByGammaSpecification(gammas)" label="Baja"  color="#0E318A"></v-switch>
         <v-switch v-model="gammas[1]" @change="filterByGammaSpecification(gammas)" label="Media" color="#0E318A"></v-switch>        
         <v-switch v-model="gammas[2]" @change="filterByGammaSpecification(gammas)" label="Alta" color="#0E318A"></v-switch>        
