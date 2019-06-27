050a61dcc1cf5ed0aac6b005aa664ef2b6e684f3
Alejandro <alejandro.orostica@usach.cl>
2019-06-27 17:04:08 -0400

    Gráfico añadido

--- frontend/src/components/devices/Twitteros.vue
+++ frontend/src/components/devices/Twitteros.vue
@@ -1,63 +1,92 @@
 <template>
-    <VLayout row wrap class>
-    <v-flex xs12 md4 v-for="(aux, index) in listaaux" :key="index">
-  <v-card
-    class="mx-auto; mt-4"
-    color="#26c6da"
-    dark
-    
-  >
-    <v-card-title>
-      <twitter-icon
-      large
-      left/>
-      <span class="title font-weight-light">Twitter</span>
-    </v-card-title>
-
-    <v-card-text class="headline font-weight-bold">
-      "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
-    </v-card-text>
-
-    <v-card-actions>
-      <v-list-tile class="grow">
-        <v-list-tile-avatar color="grey darken-3">
-          <v-img
-            class="elevation-6"
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></v-img>
-        </v-list-tile-avatar>
-
-        <v-list-tile-content>
-          <v-list-tile-title>Evan You</v-list-tile-title>
-        </v-list-tile-content>
-
-        <v-layout
-          align-center
-          justify-end
-          ma-1
-        >
-          <v-icon class="mr-1">favorite</v-icon>
-          <span class="subheading mr-2">256</span>
-          <v-icon class="mr-1">share</v-icon>
-          <span class="subheading">45</span>
-        </v-layout>
-      </v-list-tile>
-    </v-card-actions>
-  </v-card>
-    </v-flex>
-    </VLayout>   
+     <v-container grid-list-xl>
+    <v-layout row wrap>
+      <v-flex md12>
+        <highcharts :options="getData()"></highcharts>
+      </v-flex>
+      
+    </v-layout>
+  </v-container>   
 </template>
 
 <script>
+import { mapState,mapMutations, Store } from 'vuex';
 import TwitterIcon from "vue-material-design-icons/Twitter.vue"
+
+
+
   export default {
+    name: 'Charts1',
     components:{
       TwitterIcon,
     },
+    computed:{
+    //...mapState([]),
+    },
+
+    methods:{
+      //...mapMutations([]),
+
+      getData(){
+        var chartOptions = {
+        responsive: {
+        rules: [{
+            condition: {
+            maxWidth: 500
+            },
+            chartOptions: {
+            legend: {
+                enabled: false
+            }
+            }
+        }]
+        },
+        chart: {
+          
+          renderTo: 'cointainer',
+          type: 'column'
+        },
+        title: {
+          text: 'Evaluación de Celulares por Peso',
+          x:0,
+          y:7
+        },
+        xAxis: {
+          categories: ['Cel1', 'Cel2', 'Cel3', 'Cel4', 'Cel5', 'Cel6', 'Cel7'],
+    
+        },
+        yAxis:{
+          title: {
+            text: 'Peso'
+          }
+
+        },
+        legend: {
+          align: 'top',
+          verticalAlign: 'top',
+          layout: 'horizontal',
+          x: 100,
+          y: 20
+        },
+        plotOptions: {
+          column:{
+            borderRadius: 4
+          },
+        },
+        series: [
+          {
+          data: [1,2,3,4,5,6,7],
+          name:'Evaluación de marcas',
+          color: 'orange'
+          }]
+      }
+      return chartOptions;
+      }
+
+    },
+    
     data(){
-			return{
-			listaaux:[1,2,3,4,5]
-			}
+			
     }
   }
 </script>
