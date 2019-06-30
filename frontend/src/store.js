1867a42d9009bdb8af08ff7083c2caaf30676615
Vale <valentina.ligueno@gmail.com>
2019-06-29 21:42:38 -0400

    No recuerdo q es

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -29,14 +29,14 @@ export default new Vuex.Store({
       {spec: 'Almacenamiento', icon:'storage'},
       {spec: 'Bateria: ', icon:'battery_charging_full'}],
     topTen: {
-        evalP:[],
-        evalN:[],
-        evalNeutral:[],
-        topTenNames: [],
-        topTenImgList: [],
-        topTenEvalSpecification: [],
-        topTenSpecData:[]
-      },
+      evalP:[],
+      evalN:[],
+      evalNeutral:[],
+      topTenNames: [],
+      topTenImgList: [],
+      topTenEvalSpecification: [],
+      topTenSpecData:[]
+    },
     brandData: {
       brandImgList: [],
       brandNames:[],  
@@ -45,10 +45,13 @@ export default new Vuex.Store({
       evalN:[],
       evalNeutral:[],
     },
+    allGraphData:{nodes:[], links: []},
+    graphData: {nodes:[], links: []},
+    link:[],
     ready:0,
   },
   mutations: {
-    //Button bar
+    /*Verificacion de elemento activo en ChangeToolbar*/
     changeActive(state, newStatus, view){
         state.active = newStatus
     },
@@ -56,14 +59,18 @@ export default new Vuex.Store({
     {
         state.active = 'graph'
     },
-    
+
+
+    /*Traer todo de la DB*/
     async getAllAll(state){
       try{
         var phoneSpecification = []
         await Axios 
         .get('http://localhost:8081/phones/getall')
         .then(response => (state.phoneData = response.data))
-        console.log(state.phoneData)
+        
+        console.log('phoneData con datos cargados')
+
         for (let index = 1; index < 7; index++) {
           var listFilter = []
           await Axios
@@ -71,133 +78,29 @@ export default new Vuex.Store({
           .then(response => (listFilter = response.data))   
           state.phoneSpecification.push(listFilter);
         }
-        console.log(state.phoneSpecification)
+        console.log('phoneSpecification con datos cargados')
 
         await Axios
         .get('http://localhost:8081/brands/')
         .then(response => (state.brandList = response.data))
+        
+        console.log('brandList con datos cargados')
 
-        state.ready = 1
+        /*
+        await Axios
+        .get('http://localhost:8081/neo/fullNodos/')
+        .then(response => (state.allGraphData.nodes = response.data))
+
+        await Axios
+        .get('http://localhost:8081/neo/fullAristas/')
+        .then(response => (state.allGraphData.links = response.data))
+        */
+
       }catch(err){console.log("En get all all " + err)}
     },
-    async filterBySpecification(state, specification_id)
-    {
-      var names = []
-      var imgList = []
-      var evalSpecification = []
-      var specData = []
-      var phonesDescription = []
-      var topTen = {
-        evalP:[],
-        evalN:[],
-        evalNeutral:[],
-        topTenNames: [],
-        topTenImgList: [],
-        topTenEvalSpecification: [],
-        topTenSpecData:[]
-      }
-      var index = 0
 
-      for(var item of state.phoneSpecification[specification_id-1])
-      {
-        var dataSheet = []
-        dataSheet.push(item.phone.data_sheet.cpu)
-        dataSheet.push(item.phone.data_sheet.ram)
-        dataSheet.push(item.phone.data_sheet.operative_s)
-        dataSheet.push(item.phone.data_sheet.dimensions)
-        dataSheet.push(item.phone.data_sheet.front_cam)
-        dataSheet.push(item.phone.data_sheet.back_cam)
-        dataSheet.push(item.phone.data_sheet.screen)
-        dataSheet.push(item.phone.data_sheet.storage)
-        dataSheet.push(item.phone.data_sheet.batery)
-        phonesDescription.push(item.phone.description)
-          
-        if(index >= 10){
-          specData.push(dataSheet);
-          evalSpecification.push(item.assessment)
-          names.push(item.phone.model)
-          imgList.push(item.phone.image)
-        }
-        else {
-          topTen.topTenEvalSpecification.push(item.assessment)
-          topTen.evalP.push(item.statistic.positive_density)
-          topTen.evalN.push(item.statistic.negative_density)
-          topTen.evalNeutral.push(item.statistic.neutral_density)
-          topTen.topTenNames.push(item.phone.model)
-          topTen.topTenSpecData.push(dataSheet);
-          topTen.topTenImgList.push(item.phone.image)
-          index ++
-        }
-      }
-      state.activeSpecification = state.phoneSpecification[specification_id-1][0].specification.name
-      state.activeSpecificationIndex = specification_id-1
-      state.evalSpecification = evalSpecification
-      state.names = names
-      state.imgList = imgList
-      state.specData = specData
-      state.phonesDescription = phonesDescription
-      state.topTen = topTen
-        
-    },
-    async getAllSpecification(state)
-    {
-      var names = []
-      var imgList = []
-      var evalSpecification = []
-      var specData = []
-      var phonesDescription = []
-      var topTen = {
-        evalP:[],
-        evalN:[],
-        evalNeutral:[],
-        topTenNames: [],
-        topTenImgList: [],
-        topTenEvalSpecification: [],
-        topTenSpecData:[]
-      }
-      var index = 0
-       
-      for(var item of state.phoneSpecification[0] ){
-        var dataSheet = []
-        dataSheet.push(item.phone.data_sheet.cpu)
-        dataSheet.push(item.phone.data_sheet.ram)
-        dataSheet.push(item.phone.data_sheet.operative_s)
-        dataSheet.push(item.phone.data_sheet.dimensions)
-        dataSheet.push(item.phone.data_sheet.front_cam)
-        dataSheet.push(item.phone.data_sheet.back_cam)
-        dataSheet.push(item.phone.data_sheet.screen)
-        dataSheet.push(item.phone.data_sheet.storage)
-        dataSheet.push(item.phone.data_sheet.batery)
-        phonesDescription.push(item.phone.description)
 
-        if (index > 9){
-          specData.push(dataSheet);
-          evalSpecification.push(item.assessment)
-          names.push(item.phone.model)
-          imgList.push(item.phone.image)
-
-        }
-        else{
-          topTen.evalP.push(item.statistic.positive_density)
-          topTen.evalN.push(item.statistic.negative_density)
-          topTen.evalNeutral.push(item.statistic.neutral_density)
-          topTen.topTenEvalSpecification.push(item.assessment)
-          topTen.topTenNames.push(item.phone.model)
-          topTen.topTenImgList.push(item.phone.image)
-          topTen.topTenSpecData.push(dataSheet)
-          index++
-        }
-      }
-      state.activeSpecification = state.phoneSpecification[0][0].specification.name
-      state.activeSpecificationIndex = 0
-      state.evalSpecification = evalSpecification
-      state.names = names
-      state.imgList = imgList
-      state.specData = specData;
-      state.phonesDescription = phonesDescription
-      state.topTen = topTen
-    },
-    
+    /*Funciones para vista SearchDevices*/
     async getAll(state){
       var evalSpecification = []
       var names = []
@@ -253,89 +156,8 @@ export default new Vuex.Store({
       state.imgList = imgList
       state.phonesDescription = phonesDescription
       state.topTen = topTen
-
-      },
-    async getBrands(state){
-      var brandData = {
-        brandImgList: [],
-        brandNames:[],  
-        evalBrand: [],
-        evalP:[],
-        evalN:[],
-        evalNeutral:[],
-      }
-
-      for(var item of state.brandList){
-        brandData.evalBrand.push(item.assessment)
-        brandData.brandNames.push(item.name)
-        brandData.brandImgList.push(item.image)
-        brandData.evalP.push(item.statistic.positive_density)
-        brandData.evalN.push(item.statistic.negative_density)
-        brandData.evalNeutral.push(item.statistic.neutral_density)
-      }
-      state.brandData = brandData
     },
-    filterByGammaSpecification(state,gammas){
-      var evalSpecification = []
-      var names = []
-      var imgList = []
-      var specData = []
-      var phonesDescription = []
-      var topTen = {
-        evalP:[],
-        evalN:[],
-        evalNeutral:[],
-        topTenNames: [],
-        topTenImgList: [],
-        topTenEvalSpecification: [],
-        topTenSpecData:[]
-      }
-      var index = 0
 
-      for(var item of state.phoneSpecification[state.activeSpecificationIndex]){
-        var dataSheet = []
-
-          if(gammas[item.phone.gamma.gammaId - 1]){            
-            dataSheet.push(item.phone.data_sheet.cpu)
-            dataSheet.push(item.phone.data_sheet.ram)
-            dataSheet.push(item.phone.data_sheet.operative_s)
-            dataSheet.push(item.phone.data_sheet.dimensions)
-            dataSheet.push(item.phone.data_sheet.front_cam)
-            dataSheet.push(item.phone.data_sheet.back_cam)
-            dataSheet.push(item.phone.data_sheet.screen)
-            dataSheet.push(item.phone.data_sheet.storage)
-            dataSheet.push(item.phone.data_sheet.batery)
-            phonesDescription.push(item.phone.description)
-
-            
-            if(index > 9){
-              evalSpecification.push(item.assessment)
-              names.push(item.phone.model)
-              imgList.push(item.phone.image)
-              specData.push(dataSheet);
-
-            }
-            else{
-              topTen.evalP.push(item.statistic.positive_density)
-              topTen.evalN.push(item.statistic.negative_density)
-              topTen.evalNeutral.push(item.statistic.neutral_density)
-              topTen.topTenEvalSpecification.push(item.assessment)
-              topTen.topTenNames.push(item.phone.model)
-              topTen.topTenImgList.push(item.phone.image)
-              topTen.topTenSpecData.push(dataSheet)
-              index ++
-            }
-          }
-        }
-        state.dataSheet = dataSheet
-        state.activeSpecification = state.phoneSpecification[state.activeSpecificationIndex][0].specification.name
-        state.evalSpecification = evalSpecification
-        state.names = names
-        state.imgList = imgList
-        state.specData = specData
-        state.phonesDescription = phonesDescription
-        state.topTen = topTen
-    },
     filterByGama(state,gammas){
       var names = []
       var imgList = []
@@ -399,6 +221,215 @@ export default new Vuex.Store({
       state.phonesDescription = phonesDescription
       state.topTen = topTen
     },
+    
+
+    /*Funciones para vista SearchSpecification*/
+    async filterBySpecification(state, specification_id)
+    {
+      var names = []
+      var imgList = []
+      var evalSpecification = []
+      var specData = []
+      var phonesDescription = []
+      var topTen = {
+        evalP:[],
+        evalN:[],
+        evalNeutral:[],
+        topTenNames: [],
+        topTenImgList: [],
+        topTenEvalSpecification: [],
+        topTenSpecData:[]
+      }
+      var index = 0
+
+      for(var item of state.phoneSpecification[specification_id-1])
+      {
+        var dataSheet = []
+        dataSheet.push(item.phone.data_sheet.cpu)
+        dataSheet.push(item.phone.data_sheet.ram)
+        dataSheet.push(item.phone.data_sheet.operative_s)
+        dataSheet.push(item.phone.data_sheet.dimensions)
+        dataSheet.push(item.phone.data_sheet.front_cam)
+        dataSheet.push(item.phone.data_sheet.back_cam)
+        dataSheet.push(item.phone.data_sheet.screen)
+        dataSheet.push(item.phone.data_sheet.storage)
+        dataSheet.push(item.phone.data_sheet.batery)
+        phonesDescription.push(item.phone.description)
+          
+        if(index >= 10){
+          specData.push(dataSheet);
+          evalSpecification.push(item.assessment)
+          names.push(item.phone.model)
+          imgList.push(item.phone.image)
+        }
+        else {
+          topTen.topTenEvalSpecification.push(item.assessment)
+          topTen.evalP.push(item.statistic.positive_density)
+          topTen.evalN.push(item.statistic.negative_density)
+          topTen.evalNeutral.push(item.statistic.neutral_density)
+          topTen.topTenNames.push(item.phone.model)
+          topTen.topTenSpecData.push(dataSheet);
+          topTen.topTenImgList.push(item.phone.image)
+          index ++
+        }
+      }
+      state.activeSpecification = state.phoneSpecification[specification_id-1][0].specification.name
+      state.activeSpecificationIndex = specification_id-1
+      state.evalSpecification = evalSpecification
+      state.names = names
+      state.imgList = imgList
+      state.specData = specData
+      state.phonesDescription = phonesDescription
+      state.topTen = topTen
+        
+    },
+
+    async getAllSpecification(state)
+    {
+      var names = []
+      var imgList = []
+      var evalSpecification = []
+      var specData = []
+      var phonesDescription = []
+      var topTen = {
+        evalP:[],
+        evalN:[],
+        evalNeutral:[],
+        topTenNames: [],
+        topTenImgList: [],
+        topTenEvalSpecification: [],
+        topTenSpecData:[]
+      }
+      var index = 0
+       
+      for(var item of state.phoneSpecification[0] ){
+        var dataSheet = []
+        dataSheet.push(item.phone.data_sheet.cpu)
+        dataSheet.push(item.phone.data_sheet.ram)
+        dataSheet.push(item.phone.data_sheet.operative_s)
+        dataSheet.push(item.phone.data_sheet.dimensions)
+        dataSheet.push(item.phone.data_sheet.front_cam)
+        dataSheet.push(item.phone.data_sheet.back_cam)
+        dataSheet.push(item.phone.data_sheet.screen)
+        dataSheet.push(item.phone.data_sheet.storage)
+        dataSheet.push(item.phone.data_sheet.batery)
+        phonesDescription.push(item.phone.description)
+
+        if (index > 9){
+          specData.push(dataSheet);
+          evalSpecification.push(item.assessment)
+          names.push(item.phone.model)
+          imgList.push(item.phone.image)
+
+        }
+        else{
+          topTen.evalP.push(item.statistic.positive_density)
+          topTen.evalN.push(item.statistic.negative_density)
+          topTen.evalNeutral.push(item.statistic.neutral_density)
+          topTen.topTenEvalSpecification.push(item.assessment)
+          topTen.topTenNames.push(item.phone.model)
+          topTen.topTenImgList.push(item.phone.image)
+          topTen.topTenSpecData.push(dataSheet)
+          index++
+        }
+      }
+      state.activeSpecification = state.phoneSpecification[0][0].specification.name
+      state.activeSpecificationIndex = 0
+      state.evalSpecification = evalSpecification
+      state.names = names
+      state.imgList = imgList
+      state.specData = specData;
+      state.phonesDescription = phonesDescription
+      state.topTen = topTen
+    },
+
+    filterByGammaSpecification(state,gammas){
+      var evalSpecification = []
+      var names = []
+      var imgList = []
+      var specData = []
+      var phonesDescription = []
+      var topTen = {
+        evalP:[],
+        evalN:[],
+        evalNeutral:[],
+        topTenNames: [],
+        topTenImgList: [],
+        topTenEvalSpecification: [],
+        topTenSpecData:[]
+      }
+      var index = 0
+
+      for(var item of state.phoneSpecification[state.activeSpecificationIndex]){
+        var dataSheet = []
+
+          if(gammas[item.phone.gamma.gammaId - 1]){            
+            dataSheet.push(item.phone.data_sheet.cpu)
+            dataSheet.push(item.phone.data_sheet.ram)
+            dataSheet.push(item.phone.data_sheet.operative_s)
+            dataSheet.push(item.phone.data_sheet.dimensions)
+            dataSheet.push(item.phone.data_sheet.front_cam)
+            dataSheet.push(item.phone.data_sheet.back_cam)
+            dataSheet.push(item.phone.data_sheet.screen)
+            dataSheet.push(item.phone.data_sheet.storage)
+            dataSheet.push(item.phone.data_sheet.batery)
+            phonesDescription.push(item.phone.description)
+
+            
+            if(index > 9){
+              evalSpecification.push(item.assessment)
+              names.push(item.phone.model)
+              imgList.push(item.phone.image)
+              specData.push(dataSheet);
+
+            }
+            else{
+              topTen.evalP.push(item.statistic.positive_density)
+              topTen.evalN.push(item.statistic.negative_density)
+              topTen.evalNeutral.push(item.statistic.neutral_density)
+              topTen.topTenEvalSpecification.push(item.assessment)
+              topTen.topTenNames.push(item.phone.model)
+              topTen.topTenImgList.push(item.phone.image)
+              topTen.topTenSpecData.push(dataSheet)
+              index ++
+            }
+          }
+        }
+        state.dataSheet = dataSheet
+        state.activeSpecification = state.phoneSpecification[state.activeSpecificationIndex][0].specification.name
+        state.evalSpecification = evalSpecification
+        state.names = names
+        state.imgList = imgList
+        state.specData = specData
+        state.phonesDescription = phonesDescription
+        state.topTen = topTen
+    },
+
+
+    /*Funciones para vista SearchBrands*/
+    async getBrands(state){
+      var brandData = {
+        brandImgList: [],
+        brandNames:[],  
+        evalBrand: [],
+        evalP:[],
+        evalN:[],
+        evalNeutral:[],
+      }
+
+      for(var item of state.brandList){
+        brandData.evalBrand.push(item.assessment)
+        brandData.brandNames.push(item.name)
+        brandData.brandImgList.push(item.image)
+        brandData.evalP.push(item.statistic.positive_density)
+        brandData.evalN.push(item.statistic.negative_density)
+        brandData.evalNeutral.push(item.statistic.neutral_density)
+      }
+      state.brandData = brandData
+    },
+    
+    
+    /*Funciones para vista NetworkGraph*/
   },
   actions: {
     getAll (context){
